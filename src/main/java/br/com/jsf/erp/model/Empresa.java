c72f682b7ea9bfa33b56ff7ad6bd1032e0db77aa
Isac Kaik <isackaik1@gmail.com>
2023-03-17 16:31:20 -0300

    criado classes inicias

---
+++ src/main/java/br/com/jsf/erp/model/Empresa.java
@@ -0,0 +1,119 @@
+package br.com.jsf.erp.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table (name = "empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome_fantasia")
+	private String nomeFantasia;
+	
+	@Column(name = "razao_social")
+	private String razaoSocia;
+	
+	private String cnpj;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data_funcacao")
+	private Date dataFundacao;
+	
+	@ManyToOne
+	@JoinColumn(name = "ramo_atividade_id")
+	private RamoAtividade ramoAtividade;
+
+	@Enumerated(EnumType.STRING)
+	private TipoEmpresa tipo;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocia() {
+		return razaoSocia;
+	}
+
+	public void setRazaoSocia(String razaoSocia) {
+		this.razaoSocia = razaoSocia;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getDataFundacao() {
+		return dataFundacao;
+	}
+
+	public void setDataFundacao(Date dataFundacao) {
+		this.dataFundacao = dataFundacao;
+	}
+
+	public RamoAtividade getRamoAtividade() {
+		return ramoAtividade;
+	}
+
+	public void setRamoAtividade(RamoAtividade ramoAtividade) {
+		this.ramoAtividade = ramoAtividade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empresa other = (Empresa) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + "]";
+	}
+	
+}
