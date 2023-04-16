49ad17765c36abe5d58c3e2a321682a82e56664b
Isac Kaik <isackaik1@gmail.com>
2023-04-16 00:46:01 -0300

    Implementando a função de pesquisa por empresa

--- src/main/java/br/com/jsf/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/br/com/jsf/erp/controller/GestaoEmpresasBean.java
@@ -9,24 +9,46 @@ import javax.inject.Named;
 
 import br.com.jsf.erp.model.Empresa;
 import br.com.jsf.erp.repository.EmpresaRepository;
+import br.com.jsf.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EmpresaRepository empresaRepository;
-	
+
+	@Inject
+	private FacesMessages messages;
+
 	private List<Empresa> listaEmpresas;
-	
+
+	private String termoPesquisa;
+
+	public void pesquisar() {
+		listaEmpresas = empresaRepository.pesquisar(termoPesquisa);
+
+		if (listaEmpresas.isEmpty()) {
+			messages.info("Sem registros para essa consulta.");
+		}
+	}
+
 	public void todasEmpresas() {
 		listaEmpresas = empresaRepository.todas();
 	}
-	
+
 	public List<Empresa> getListaEmpresas() {
 		return listaEmpresas;
 	}
-	
+
+	public String getTermoPesquisa() {
+		return termoPesquisa;
+	}
+
+	public void setTermoPesquisa(String termoPesquisa) {
+		this.termoPesquisa = termoPesquisa;
+	}
+
 }
--- src/main/java/br/com/jsf/erp/repository/EmpresaRepository.java
+++ src/main/java/br/com/jsf/erp/repository/EmpresaRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.jsf.erp.model.Empresa;
 
@@ -25,6 +26,17 @@ public class EmpresaRepository implements Serializable {
 
 	public Empresa findById(Long id) {
 		return manager.find(Empresa.class, id);
+	}
+	
+	public List<Empresa> pesquisar(String nome) {
+		String jpql = "from Empresa where razaoSocial like :razaoSocial";
+		
+		TypedQuery<Empresa> query = manager
+				.createQuery(jpql, Empresa.class);
+		
+		query.setParameter("razaoSocial", nome + "%");
+		
+		return query.getResultList();
 	}
 
 	public List<Empresa> todas() {
